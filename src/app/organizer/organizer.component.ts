89fa6001350cdd6753f9e4efa4b07289173b062a
Innerbloom <artm.mishurovskiy@gmail.com>
2022-09-09 23:53:03 +0300

    End Calendar + DB

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -12,11 +12,17 @@ import {switchMap} from 'rxjs/operators';
 export class OrganizerComponent implements OnInit {
 
   form!: FormGroup;
+  tasks: Task[] = [];
 
   constructor(public dateService: DateService,
               private tasksService: TasksService) { }
 
   ngOnInit(): void {
+    this.dateService.date.pipe(switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
+
     this.form = new FormGroup( {
       title: new FormControl ('', Validators.required)
     })
@@ -30,10 +36,14 @@ export class OrganizerComponent implements OnInit {
       date: this.dateService.date.value.format('DD-MM-YYYY')
     }
     this.tasksService.create(task).subscribe(task => {
+      this.tasks.push(task)
       this.form.reset()
     }, error => console.error(error))
 
-
   }
-
+  remove(task: Task) {
+    this.tasksService.remove(task).subscribe( () => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    }, err => console.error(err))
+      }
 }
--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -10,6 +10,10 @@ export interface Task {
   date?: string
 }
 
+interface CreateResponse {
+  name: string
+}
+
 @Injectable({providedIn: 'root'})
 export class TasksService {
   static url = 'https://angular-practice-calenda-f83ca-default-rtdb.firebaseio.com/'
@@ -17,13 +21,27 @@ export class TasksService {
   constructor(private http: HttpClient) {
   }
 
+  load(date: moment.Moment): Observable<Task[]> {
+    return this.http
+      .get<Task[]>(`${TasksService.url}/${date.format('DD-MM-YYYY')}.json`)
+      .pipe(map(tasks => {
+        if (!tasks) {
+          return []
+        }
+        return Object.keys(tasks).map((key: any) => ({...tasks[key], id: key}))
+      }))
+  }
+
   create(task: Task): Observable<Task> {
     return this.http
-      .post<any>(`${TasksService.url}/${task.date}.json`, task)
+      .post<CreateResponse>(`${TasksService.url}/${task.date}.json`, task)
       .pipe(map(res => {
         return {...task, id: res.name}
       }))
   }
-
+  remove(task: Task) {
+    return this.http
+      .delete<void>(`${TasksService.url}/${task.date}/${task.id}.json`)
+  }
 
 }
